9b385037de85e18e731b323ac76df8a03ad729d4
rafaelofficial <rafael.sousa.pereira@mail.usf.edu.br>
2021-04-23 17:54:24 -0300

    final layout

---
+++ lib/challenge/challenge_page.dart
@@ -0,0 +1,28 @@
+import 'package:dev_quiz/challenge/widgets/question_indicator/question_indicator.dart';
+import 'package:flutter/material.dart';
+
+import 'widgets/quiz/quiz_widget.dart';
+
+class ChallengePage extends StatefulWidget {
+  @override
+  _ChallengePageState createState() => _ChallengePageState();
+}
+
+class _ChallengePageState extends State<ChallengePage> {
+  @override
+  Widget build(BuildContext context) {
+    // Challenge Page
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(60),
+        child: SafeArea(
+          top: true,
+          child: QuestionIndicatorWidget(),
+        ),
+      ),
+      body: QuizWidget(
+        title: "O que o Flutter faz em sua totalidade?",
+      ),
+    );
+  }
+}
