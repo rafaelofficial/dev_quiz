fd43234e8c47347077988e24db8ba3fa9bfbedc2
rafaelofficial <rafael.sousa.pereira@mail.usf.edu.br>
2021-04-25 07:05:55 -0300

    ending the animation

--- lib/home/widgets/app_bar/app_bar_widget.dart
+++ lib/home/widgets/app_bar/app_bar_widget.dart
@@ -44,7 +44,12 @@ class AppBarWidget extends PreferredSize {
                     ],
                   ),
                 ),
-                Align(alignment: Alignment(0.0, 1.0), child: ScoreCardWidget()),
+                Align(
+                  alignment: Alignment(0.0, 1.0),
+                  child: ScoreCardWidget(
+                    percent: user.score / 100,
+                  ),
+                ),
               ],
             ),
           ),
